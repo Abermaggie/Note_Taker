5c53d22643a0cd03a904c0995c217b74e8eaa1f8
Maggie Abernathy <mlabernathy05@gmail.com>
2023-01-03 14:04:16 -0600

    server.js file updated with imports, parsing, and get calls

---
+++ Develop/Routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const newNotes = require('./newNotes');
+const savedNotes = require('./savedNotes');
+
+const app = express();
+
+
+
+
+
+
+
+module.exports = api;
--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,25 @@
 // This will be the jumping off point for file direction and starting the port to listen//
+
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api', api);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`Application listening at http://localhost:${PORT}`)
+);
